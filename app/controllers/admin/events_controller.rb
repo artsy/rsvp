7767e8004c314245c2afef3b6fdab72386a338b6
Charles Broskoski <broskoski@gmail.com>
2016-10-29 22:48:55 -0400

    Add new event button and clean up fields a bit

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -34,7 +34,7 @@ class Admin::EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to admin_events_url, notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
