9b0dad3f283687ee24fb74b67bd94b9f239f0705
Charles Broskoski <broskoski@gmail.com>
2016-10-29 20:04:54 -0400

    Set up puma and basic auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.basic_auth_user = ENV["BASIC_AUTH_USER"]
+  config.basic_auth_password = ENV["BASIC_AUTH_PASSWORD"]
 end
