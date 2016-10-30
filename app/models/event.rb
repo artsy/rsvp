428bb4d7b236fe68dfba952426ea00ce941e7100
Charles Broskoski <broskoski@gmail.com>
2016-10-29 21:48:32 -0400

    Update thank you controller, strip guests array

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,8 @@ class Event < ApplicationRecord
   def rsvp_count
     rsvps.count + rsvps.inject(0) { |n, rsvp| rsvp.guests.length + n }
   end
+
+  def is_over_capacity
+    rsvp_count > capacity
+  end
 end
