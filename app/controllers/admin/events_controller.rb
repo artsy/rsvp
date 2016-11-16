04375741b4a9d96a0f2e1027fddd64edc4ee37e3
Charles Broskoski <broskoski@gmail.com>
2016-11-16 16:25:50 -0500

    Add jquery-ui and correct redirect link in destroy action

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -62,7 +62,7 @@ class Admin::EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to admin_events_url, notice: 'Event was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
