6bf238148eca08838a90d3395f3e700fe8e682bd
Eloy Durán <eloy.de.enige@gmail.com>
2016-12-02 14:52:45 -0500

    Render event description as markdown.

--- app/helpers/rsvps_helper.rb
+++ app/helpers/rsvps_helper.rb
@@ -1,2 +1,7 @@
 module RsvpsHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, no_images: true, no_styles: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(text).html_safe
+  end
 end
