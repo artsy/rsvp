cbc9f20ea7174f51b9a167577c985b1f6413f215
Matthew Zikherman <matt@bestvendor.com>
2017-05-31 17:31:27 -0400

    Add little spec around constellation API calls

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem 'redcarpet'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails'
+  gem 'webmock'
 end
 
 group :development do
---
+++ spec/lib/constellation_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Constellation module' do
+  
+  context 'requests' do
+    before do
+      allow(RsvpRails).to receive(:config).and_return(jwt_token: 'token', constellation_url: 'https://example.com')
+    end
+    let(:rsvp_params) { { name: 'Matt', email: 'matt@email.com' } }
+    let(:successful_stub) { stub_request(:post, 'https://example.com/rsvps').to_return(body: '{}', status: 201) }
+    let(:unsuccessful_stub) { stub_request(:post, 'https://example.com/rsvps').to_return(body: 'uh oh', status: 400) }
+    it 'makes the right POST request with headers and body' do
+      successful_stub
+      Constellation.create_rsvp!(rsvp_params)
+      expect(successful_stub).to have_been_requested
+    end
+    it 'raises an exception on an unsuccessful rsvp create' do
+      unsuccessful_stub
+      expect do
+        Constellation.create_rsvp!(rsvp_params)
+      end.to raise_error(ConstellationHttpException) do |e|
+        expect(e.message).to eq 'uh oh'
+      end
+    end
+  end
+end
