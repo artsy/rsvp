be46211b35151070cc8118adac52103a7273a4b8
Matthew Zikherman <matt@bestvendor.com>
2017-06-08 12:09:38 -0400

    Fully drop the rsvps table and model for deprecation

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,4 @@
 module EventsHelper
-  def count_to_capacity(event)
-    "#{event.rsvp_count} / #{event.capacity}"
-  end
-
   def formatted_date(date)
     return 'N/A' if date.nil?
     date.strftime("%m/%d/%Y – %I:%M%p")
